efb47455e5a0290710bf3280a60216d03bf9abb0
juliwithoutthee <jnboucree@gmail.com>
2022-05-30 11:17:45 +0900

    Creates tasks controller

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,15 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { UsersController } from './users/users.controller';
+import { UsersService } from './users/users.service';
+import { EventsController } from './events/events.controller';
+import { EventsService } from './events/events.service';
+import { TasksController } from './tasks/tasks.controller';
 
 @Module({
   imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  controllers: [AppController, UsersController, EventsController, TasksController],
+  providers: [AppService, UsersService, EventsService],
 })
 export class AppModule {}
