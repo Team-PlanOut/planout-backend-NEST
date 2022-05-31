aefc865768cf787f24311c9a7fd281ed46a460dd
juliwithoutthee <jnboucree@gmail.com>
2022-05-31 17:05:13 +0900

    Updates tasks controller with placeholder methods and routes

--- src/tasks/tasks.controller.ts
+++ src/tasks/tasks.controller.ts
@@ -1,4 +1,40 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Delete, Get, Post, Put } from '@nestjs/common';
 
 @Controller('tasks')
-export class TasksController {}
+export class TasksController {
+
+    @Get()
+    getAllTasks() {
+
+    }
+
+    @Get('/:id')
+    getTaskById() { }
+
+    @Get('/event/:id')
+    getTasksByEvent() {
+
+    }
+
+    @Get('/user/:id')
+    getTasksByUser() {
+
+    }
+
+    @Post('/')
+    saveTask() {
+
+    }
+
+    @Put('/:id')
+    updateTasks() {
+
+    }
+
+    @Delete('/:id')
+    deleteTask() {
+        
+    }
+
+
+}
