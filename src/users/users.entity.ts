c2ae5d3b9f93dde7b4359b7212f22a264f433b6b
juliwithoutthee <jnboucree@gmail.com>
2022-05-31 17:07:02 +0900

    Users Entity in progress

--- src/users/users.entity.ts
+++ src/users/users.entity.ts
@@ -1,9 +1,10 @@
+import { type } from "os";
 import { Events } from "src/events/events.entity";
 import { Tasks } from "src/tasks/tasks.entity";
-import { Column, Entity, JoinTable, ManyToMany, OneToMany, PrimaryColumn, PrimaryGeneratedColumn } from "typeorm";
+import { BaseEntity, Column, Entity, JoinTable, ManyToMany, ManyToOne, OneToMany, PrimaryColumn, PrimaryGeneratedColumn } from "typeorm";
 
 @Entity()
-export class Users {
+export class Users extends BaseEntity {
 
     @PrimaryGeneratedColumn()
     id: string;
@@ -18,17 +19,17 @@ export class Users {
     lastName: string;
 
     @Column()
-    friendsList: [];
+    friendsList: []; // check and fix later
 
     @Column()
     points: number;
 
-    @ManyToMany(type => Events) @JoinTable()
+    @OneToMany(type => Events, events=>Events) @JoinTable() //check
     events: Events[]; 
 
-    @ManyToMany(type => Tasks) @JoinTable()
+    @OneToMany(type => Tasks, tasks=>Tasks) @JoinTable() //check
     tasks: Tasks[];
 
-    
+
     //check if typeorm has self generated created and update times
 }
