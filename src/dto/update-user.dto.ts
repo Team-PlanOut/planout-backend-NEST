3fdead18bc709b0253629d718cc55e570b2c1c42
juliwithoutthee <jnboucree@gmail.com>
2022-06-01 19:05:21 +0900

    Updates update-user.dto
    Co-authored-by: constantine7s <constantine7s@users.noreply.github.com>
    Co-authored-by: sodajyu <sodajyu@users.noreply.github.com>

---
+++ src/dto/update-user.dto.ts
@@ -0,0 +1,8 @@
+export default class UpdateUserDto {
+    readonly firstName: string;
+    readonly lastName: string;
+    readonly points: number; 
+    readonly email: string;
+
+    //user columns for creating a new user 
+}
