9ddd6405548ac4c0127c4493bcf346ecf0766a9b
juliwithoutthee <jnboucree@gmail.com>
2022-06-01 15:41:38 +0900

    Updates events entity

--- src/events/events.entity.ts
+++ src/events/events.entity.ts
@@ -1,6 +1,6 @@
 import { Tasks } from "src/tasks/tasks.entity";
 import { Users } from "src/users/users.entity";
-import { Column, Entity, ManyToOne, PrimaryGeneratedColumn } from "typeorm";
+import { Column, Entity, ManyToOne, PrimaryGeneratedColumn, Timestamp } from "typeorm";
 
 @Entity()
 export class Events {
@@ -9,13 +9,16 @@ export class Events {
     id: number;
 
     @Column()
-    host: string; //reference single user as host 
+    host: string; //reference single user as host  //fix this
 
     @Column()
     eventName: string;
 
     @Column()
     date: Date;
+
+    @Column("time")
+    time: Timestamp;
 
     @Column()
     budget: number;
