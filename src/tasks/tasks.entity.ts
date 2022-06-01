d984aacbb5ccb94b6486d82c11cf4a1a6a70dd4b
juliwithoutthee <jnboucree@gmail.com>
2022-06-01 18:38:29 +0900

    Updates tasks entity with constraints
    Co-authored-by: constantine7s <constantine7s@users.noreply.github.com>
    Co-authored-by: sodajyu <sodajyu@users.noreply.github.com>

--- src/tasks/tasks.entity.ts
+++ src/tasks/tasks.entity.ts
@@ -1,15 +1,19 @@
 import { Events } from "src/events/events.entity";
-import { Column, Entity, JoinTable, ManyToMany, PrimaryGeneratedColumn } from "typeorm";
+import { Column, Entity, JoinTable, ManyToMany, PrimaryGeneratedColumn, Timestamp } from "typeorm";
 
 @Entity()
 export class Tasks {
     @PrimaryGeneratedColumn()
     id: number;
 
-    @Column()
+    @Column({
+        nullable: false,
+    })
     description: string;
 
-    @Column()
+    @Column({
+        nullable: false,
+    })
     status: boolean;
 
     @Column()
@@ -18,7 +22,8 @@ export class Tasks {
     @ManyToMany(type => Events) @JoinTable()
     events: Events[];
 
-
+    @Column("datetime")
+    deadline: Date; //Date and time
 }
 
 
