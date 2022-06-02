1b17d3519e7f592532122aa1c8075204a1e6591b
juliwithoutthee <jnboucree@gmail.com>
2022-06-02 12:43:31 +0900

    Updates methods within events controller
    Co-authored-by: constantine7s <constantine7s@users.noreply.github.com>
    Co-authored-by: sodajyu <sodajyu@users.noreply.github.com>

--- src/events/events.controller.ts
+++ src/events/events.controller.ts
@@ -1,39 +1,40 @@
 import { Controller, Delete, Get, Post, Put } from '@nestjs/common';
+import UpdateEventDto from 'src/dto/update-event.dto';
+import { EventsService } from './events.service';
+
 
 @Controller('events')
 export class EventsController {
+    constructor(private readonly eventsService: EventsService) {}
     @Get('/')
     getAllEvents() {
-
+        return this.eventsService.getAllEvents();
     }
 
-    @Get()
-    getEventById() {
-
-    }
-
-    @Get('/:name') 
-    getEventByName() {
-
+    @Get('/:id')
+    getEventById(eventId) {
+        return this.eventsService.getEventById(eventId);
     }
 
     @Get('/host/:id')
-    getEventByHost() {
-
+    getEventByHost(hostId: string) {
+        return this.eventsService.getEventByHostId(hostId);
     }
 
     @Post('/')
-    saveEvent() {
-
+    saveEvent(eventDetails) {
+        return this.eventsService.insert(eventDetails);
     }
 
     @Put('/:id')
-    updateEvent(){
-
+    updateEvent(eventId: number, UpdateEventDto: UpdateEventDto){
+        return this.eventsService.updateEventById(eventId, UpdateEventDto);
     }
 
     @Delete('/:id')
-    deleteEvent() {}
-
+    deleteEvent(eventId: number) {
+        return this.eventsService.deleteEvent(eventId);
+    }
+    
 }
 
