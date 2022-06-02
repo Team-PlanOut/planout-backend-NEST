b4a315e1882dfde72484b71af2ee66e18423921b
juliwithoutthee <jnboucree@gmail.com>
2022-06-02 14:44:36 +0900

    Updates tasks service methods
    Co-authored-by: constantine7s <constantine7s@users.noreply.github.com>
    Co-authored-by: sodajyu <sodajyu@users.noreply.github.com>

--- src/tasks/tasks.service.ts
+++ src/tasks/tasks.service.ts
@@ -1,4 +1,53 @@
 import { Injectable } from '@nestjs/common';
+import { Tasks } from './tasks.entity';
+import { Users } from 'src/users/users.entity';
+import UpdateTaskDto from 'src/dto/update-task.dto';
+import CreateTaskDto from 'src/dto/create-task.dto';
+
 
 @Injectable()
-export class TasksService {}
+export class TasksService {
+    //private tasks: Tasks[] = [];
+
+    async createTask(CreateTaskDto: CreateTaskDto) {
+
+    };
+
+    async getAllTasks(): Promise<Tasks[]> {
+        return Tasks.find();
+    };
+
+    async getTaskById(taskId: number): Promise<Tasks[]> {
+        const task: Tasks = await Tasks.findOne({ where: { id: taskId } });
+        return task[0];
+    };
+
+    // need getTaskByUserId
+    async getTasksByUserId(userId: string | unknown): Promise<Tasks[]> {
+        const task: Tasks[] = await Tasks.find({ where: { users: userId } }) // need Tasks.users.id reference
+        return task;
+    }
+
+    // need getTaskByEventId
+    async getTaskByEventId(eventId: number): Promise<Tasks[]> {
+        const task: Tasks[] = await Tasks.find({ where: { id: eventId } });
+        return task;
+    }
+    // need deleteTask
+    async deleteTask(taskId: number): Promise<Tasks[]> {
+        const task: Tasks = await Tasks.findOne({ where: { id: taskId } });
+        return task[0].delete();
+
+    }
+    // need updateTask
+
+    async updateTask(id: number, UpdateTaskDto: UpdateTaskDto): Promise<Tasks[]> {
+        //const task: Tasks = await Tasks.findOne({ where: { taskId: Tasks.id } });
+        // waiting for DTO //
+        return
+    }
+    /*
+        async updateTask(UpdateTaskDto: UpdateTaskDto): Promise<Tasks[]> 
+    
+    */
+}
