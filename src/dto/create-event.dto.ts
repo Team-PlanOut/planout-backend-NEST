7cea1e18ef1f39613a30fa43b9fd6e3caf9dd2a5
juliwithoutthee <jnboucree@gmail.com>
2022-06-02 12:41:16 +0900

    Updates dto files
    Co-authored-by: constantine7s <constantine7s@users.noreply.github.com>
    Co-authored-by: sodajyu <sodajyu@users.noreply.github.com>

--- src/dto/create-event.dto.ts
+++ src/dto/create-event.dto.ts
@@ -1,7 +1,7 @@
 export default class CreateEventDto {
-    readonly eventName: string;
-    readonly date: Date;
-    readonly time: Date; //Timestamp?
-    readonly hostId: number;
-    readonly budget: number;
+     eventName: string;
+     date: Date;
+     time: Date; //Timestamp?
+     hostId: number;
+     budget: number;
 }
--- src/dto/update-event.dto.ts
+++ src/dto/update-event.dto.ts
@@ -2,6 +2,6 @@ export default class UpdateEventDto {
     eventName: string;
     date: Date;
     time: Date; //Timestamp?
-    hostId: number;
+    host: string | number;
     budget: number;
 }
