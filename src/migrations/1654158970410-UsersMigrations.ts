8c26514dfde9434bc39fbc53cfbd57e32824dce7
juliwithoutthee <jnboucree@gmail.com>
2022-06-02 18:19:40 +0900

    TypeORM generated migration files
    Co-authored-by: constantine7s <constantine7s@users.noreply.github.com>
    Co-authored-by: sodajyu <sodajyu@users.noreply.github.com>

---
+++ src/migrations/1654158970410-UsersMigrations.ts
@@ -0,0 +1,11 @@
+import {MigrationInterface, QueryRunner} from "typeorm";
+
+export class UsersMigrations1654158970410 implements MigrationInterface {
+
+    public async up(queryRunner: QueryRunner): Promise<void> {
+    }
+
+    public async down(queryRunner: QueryRunner): Promise<void> {
+    }
+
+}
---
+++ src/migrations/1654161275478-TasksMigrations.ts
@@ -0,0 +1,11 @@
+import {MigrationInterface, QueryRunner} from "typeorm";
+
+export class TasksMigrations1654161275478 implements MigrationInterface {
+
+    public async up(queryRunner: QueryRunner): Promise<void> {
+    }
+
+    public async down(queryRunner: QueryRunner): Promise<void> {
+    }
+
+}
