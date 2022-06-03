8ba2caa8aec594a94ecfdcd26ce5b9444015f140
juliwithoutthee <jnboucree@gmail.com>
2022-06-03 10:23:15 +0900

    Updates main

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,16 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import databaseConnectionManager from './database';
 
-async function bootstrap() {
+
+
+databaseConnectionManager.connect().then(async() => {
   const app = await NestFactory.create(AppModule);
   await app.listen(3000);
-}
-bootstrap();
+})
+
+// async function bootstrap() {
+//   const app = await NestFactory.create(AppModule);
+//   await app.listen(3000);
+// }
+// bootstrap();
